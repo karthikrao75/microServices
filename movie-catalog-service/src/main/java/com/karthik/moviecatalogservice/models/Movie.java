e2a0655da65569004a3ab5877567909e01c55a52
karthikrao75 <karthik.prampalli@gmail.com>
2019-02-17 21:35:10 +0530

    Initial Commit

---
+++ movie-catalog-service/src/main/java/com/karthik/moviecatalogservice/models/Movie.java
@@ -0,0 +1,34 @@
+package com.karthik.moviecatalogservice.models;
+
+public class Movie {
+
+	public String movieId;
+	public String name;
+
+	public Movie() {
+		super();
+	}
+
+	public Movie(String movieId, String name) {
+		super();
+		this.movieId = movieId;
+		this.name = name;
+	}
+
+	public String getMovieId() {
+		return movieId;
+	}
+
+	public void setMovieId(String movieId) {
+		this.movieId = movieId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
---
+++ ratings-data-service/src/main/java/com/karthik/ratingsdataservice/resources/RatingsDataResource.java
@@ -0,0 +1,26 @@
+package com.karthik.ratingsdataservice.resources;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.karthik.ratingsdataservice.models.Rating;
+
+@RestController
+@RequestMapping("/ratingsdata")
+public class RatingsDataResource {
+
+	@RequestMapping("/{movieId}")
+	public Rating getRating(@PathVariable("movieId") String movieId) {
+		return new Rating(movieId, 4);
+	}
+
+	@RequestMapping("users/{userId}")
+	public List<Rating> getRatings(@PathVariable("userId") String userId) {
+		return Arrays.asList(new Rating("12345", 4), new Rating("456", 2));
+	}
+
+}
